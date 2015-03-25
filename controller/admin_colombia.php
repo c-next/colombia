abe016ab4b936bb88d440b46860d55455437fa19
NeoRazorX <neorazorx@gmail.com>
2015-03-25 16:06:55 +0100

    - Integrados los cambios en las extensiones necesarias. - Añadido asistente en Admin > Colombia.

---
+++ controller/admin_colombia.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of admin_colombia
+ *
+ * @author carlos
+ */
+class admin_colombia extends fs_controller
+{
+   public function __construct()
+   {
+      parent::__construct(__CLASS__, 'Colombia', 'admin');
+   }
+   
+   protected function private_core()
+   {
+      $this->share_extensions();
+      
+      if( isset($_GET['opcion']) )
+      {
+         if($_GET['opcion'] == 'moneda')
+         {
+            $this->empresa->coddivisa = 'COP';
+            if( $this->empresa->save() )
+            {
+               $this->new_message('Datos guardados correctamente.');
+            }
+         }
+         else if($_GET['opcion'] == 'pais')
+         {
+            $this->empresa->codpais = 'COL';
+            if( $this->empresa->save() )
+            {
+               $this->new_message('Datos guardados correctamente.');
+            }
+         }
+      }
+   }
+   
+   private function share_extensions()
+   {
+      $fsext = new fs_extension();
+      $fsext->name = 'puc_colombia';
+      $fsext->from = __CLASS__;
+      $fsext->to = 'contabilidad_ejercicio';
+      $fsext->type = 'fuente';
+      $fsext->text = 'PUC Colombia';
+      $fsext->params = 'plugins/colombia/extras/colombia.xml';
+      $fsext->save();
+   }
+}
